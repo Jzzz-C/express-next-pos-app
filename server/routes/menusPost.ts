e3033ba417f7cfb432aa003db0bfc7e8d29f228f
Jzz-C <ygyi8632@gmail.com>
2023-05-05 14:15:04 +0630

    feat: private routes

--- server/routes/menusPost.ts
+++ server/routes/menusPost.ts
@@ -12,14 +12,31 @@ router.post("/", async (req: Request, res: Response) => {
       const values = [name, price];
       const { rows } = await pool.query(text, values);
       res.send(rows);
-    } else if (req.method === "PUT") {
+    }
+  } catch (err) {
+    console.log("error", err);
+  }
+});
+
+router.put("/", async (req: Request, res: Response) => {
+  try {
+    if (req.method === "PUT") {
       const { name, price } = req.body.menu;
       const id = req.query.id;
       const text = `UPDATE menus SET name = $1, price = $2 WHERE id = $3 RETURNING *`;
       const values = [name, price, id];
       const { rows } = await pool.query(text, values);
       res.send(rows);
-    } else if (req.method === "GET") {
+    }
+  } catch (err) {
+    console.log("error", err);
+  }
+});
+
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    if (req.method === "GET") {
+      // console.log(req.query);
       const id = req.query.id;
       const text = `SELECT menus.id, menus.name AS menu_name, url, price, is_available AS available, locations.name AS location_name FROM menus
         INNER JOIN location_menus ON location_menus.menu_id = menus.id
@@ -29,6 +46,7 @@ router.post("/", async (req: Request, res: Response) => {
         WHERE locations.id = $1`;
       const values = [id];
       const menus = (await pool.query(text, values)).rows;
+      // console.log(menus);
       res.send({ menus });
     }
   } catch (err) {
