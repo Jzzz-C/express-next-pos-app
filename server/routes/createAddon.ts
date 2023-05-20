52bc28fe1dec78f517c13711a1da4fda6dab7d73
Jzz-C <ygyi8632@gmail.com>
2023-05-20 22:49:01 +0630

    feat: set multiple input data to database is already

--- server/routes/createAddon.ts
+++ server/routes/createAddon.ts
@@ -6,7 +6,26 @@ export default router;
 
 router.post("/", async (req: Request, res: Response) => {
   try {
-    console.log(req.body);
+    const { addonCatName, addonName, addonPrice } = req.body;
+
+    const addonCatResult = await pool.query(
+      "INSERT INTO addon_categories(category_name) values($1) RETURNING *",
+      [addonCatName]
+    );
+
+    const currentAddonCatId = addonCatResult.rows[0].id;
+
+    const addonsResult = await pool.query(
+      "INSERT INTO addon(addon_name, price) SELECT * FROM UNNEST ($1::text[], $2::int[]) RETURNING *",
+      [addonName, addonPrice]
+    );
+
+    const currentAddonIdArray = addonsResult.rows.map((addon) => {
+      return addon.id;
+    });
+
+    console.log(currentAddonCatId);
+    console.log(currentAddonIdArray);
 
     res.send("ok");
   } catch (err) {
